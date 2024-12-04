9c5f5f31c73dd76cb8b56a1690502d3df447069e
mina youssef <myoussef@uplandsoftware.com>
2024-12-04 16:28:39 +0200

    initial Repo

--- src/main/java/com/storage/file/StorageApplication.java
+++ src/main/java/com/storage/file/StorageApplication.java
@@ -1,15 +1,28 @@
 package com.storage.file;
 
+import com.storage.file.service.LeaderElectionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class StorageApplication {
+public class StorageApplication implements CommandLineRunner {
+	private final LeaderElectionService leaderElectionService;
 
-	public static void main(String[] args) {
+    public StorageApplication(LeaderElectionService leaderElectionService) {
+        this.leaderElectionService = leaderElectionService;
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(StorageApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		leaderElectionService.startLeaderElection();
 	}
 
 }
--- src/main/java/com/storage/file/service/LeaderElectionService.java
+++ src/main/java/com/storage/file/service/LeaderElectionService.java
@@ -1,26 +1,37 @@
 package com.storage.file.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@Service
-public class LeaderElectionService
-{
-    private boolean isLeader = false;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-    public void setLeader(boolean isLeader) {
-        this.isLeader = isLeader;
+import static java.lang.System.*;
+
+@Service
+public class LeaderElectionService {
+
+    private final String appName;
+    private final AtomicBoolean isLeader = new AtomicBoolean(false);
+
+    public LeaderElectionService(@Value("${spring.application.name}") String appName) {
+        this.appName = appName;
     }
 
-    public String performTask() {
-        if (isLeader) {
-            return "Task performed by the leader";
+    public void startLeaderElection() {
+        if (isLeader.compareAndSet(false, true)) {
+            out.println(appName + " is the leader now!");
+            simulateLeaderTasks();
         } else {
-            return "Not the leader, task skipped";
+            out.println(appName + " is a follower now.");
+            simulateFollowerTasks();
         }
     }
 
-    // Return the leadership status
-    public boolean isLeader() {
-        return isLeader;
+    private void simulateLeaderTasks() {
+        out.println(appName + " is performing leader tasks...");
+    }
+
+    private void simulateFollowerTasks() {
+        out.println(appName + " is performing follower tasks...");
     }
 }
